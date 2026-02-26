abbe8ad8e441acbcfa8ae0eece0bf9f78bb654fb
armandfrigo <armandfrigo@users.noreply.github.com>
2026-02-26 20:43:50 +0100

    page

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   // use relative paths when building for GitHub Pages or other static hosts
-  base: process.env.NODE_ENV === 'production' ? './' : '/',
+base: process.env.NODE_ENV === 'production' ? '/SwissSnowWeather/' : '/',
   plugins: [react()],
 })
